22e2228eedd87cf5a2e337b06faf8daacde6fcf3
Zach Farber <zfarber@Zachs-MBP.home>
2018-08-07 03:44:09 -0400

    finished The DOM

--- js/main.js
+++ js/main.js
@@ -24,12 +24,24 @@ var cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2) {
-		console.log(cardsInPlay[0] === cardsInPlay[1] ? "You found a match!" : "Sorry, try again.");
-		cardsInPlay = [];		
+		alert(cardsInPlay[0] === cardsInPlay[1] ? "You found a match!" : "Sorry, try again.");
+		cardsInPlay = [];	
 	};
 };
 
-function flipCard(cardId) {
-	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", JSON.stringify(cards[i]));
+		cardElement.addEventListener("click", function() {
+			var data = this.getAttribute("data-id");
+			cardsInPlay.push(JSON.parse(data).rank);
+			this.setAttribute("src", JSON.parse(data).cardImage);
+			checkForMatch();
+		});
+		document.querySelector("#game-board").appendChild(cardElement);
+	}
 };
+
+createBoard();
